d1a052c1d3459a234512a8a3c710ef378311ac92
Sonia Kurtz <kurtzsonia@gmail.com>
2017-11-03 18:03:51 +0100

    initial commit

---
+++ LibraryRESTfulApi/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using LibraryRESTfulApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using LibraryRESTfulApi.Services;
+
+namespace LibraryRESTfulApi
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var connectionString = Startup.Configuration["connectionsStrings:libraryApiConnectionString"];
+
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, LibraryContext libraryContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseMvc();
+        }
+    }
+}
