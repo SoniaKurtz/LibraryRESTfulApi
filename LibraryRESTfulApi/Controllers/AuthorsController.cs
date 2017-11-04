cff18c595808fc941af03895684d0bdaa2fbc4f0
Sonia Kurtz <kurtzsonia@gmail.com>
2017-11-04 13:00:45 +0100

    Added authors and books controllers, and get methods

---
+++ LibraryRESTfulApi/Controllers/AuthorsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using LibraryRESTfulApi.Helpers;
+using LibraryRESTfulApi.Models;
+using LibraryRESTfulApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryRESTfulApi.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+
+            var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+
+            return Ok(authors);    
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var authorFromRepo = _libraryRepository.GetAuthor(id);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            var author = Mapper.Map<AuthorDto>(authorFromRepo);
+
+            return Ok(author);
+        }
+    }
+}
--- LibraryRESTfulApi/Startup.cs
+++ LibraryRESTfulApi/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.Configuration;
 using LibraryRESTfulApi.Entities;
 using Microsoft.EntityFrameworkCore;
 using LibraryRESTfulApi.Services;
+using Microsoft.Extensions.Logging;
+using LibraryRESTfulApi.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace LibraryRESTfulApi
 {
@@ -21,7 +25,11 @@ namespace LibraryRESTfulApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+                setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            });
 
             var connectionString = Startup.Configuration["connectionsStrings:libraryApiConnectionString"];
 
@@ -31,12 +39,35 @@ namespace LibraryRESTfulApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, LibraryContext libraryContext)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, 
+            LibraryContext libraryContext)
         {
+            loggerFactory.AddConsole();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later");
+                    });
+                });
+            }
+
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Entities.Author, Models.AuthorDto>().
+                ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}")).
+                ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+
+                cfg.CreateMap<Entities.Book, Models.BookDto>();
+            });
 
             libraryContext.EnsureSeedDataForContext();
 
